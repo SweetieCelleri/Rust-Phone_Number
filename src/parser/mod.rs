2f4d8c68150c37829f0742ca48d863e386958051
Lucas MIALON <lulum366@gmail.com>
2026-04-27 22:32:58 +0200

    feat(parser): complete parser

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,7 +1,21 @@
 use serde::Deserialize;
+use std::fs;
+use std::path::Path;
 
 #[derive(Debug, Deserialize, PartialEq)]
 pub struct Contact {
     pub nb: String,
     pub name: String,
+}
+
+pub fn parse_contacts(json_content: &str) -> Result<Vec<Contact>, serde_json::Error> {
+    serde_json::from_str(json_content)
+}
+
+pub fn load_contacts_from_file<P: AsRef<Path>>(
+    path: P,
+) -> Result<Vec<Contact>, Box<dyn std::error::Error>> {
+    let json_content = fs::read_to_string(path)?;
+    let contacts = parse_contacts(&json_content)?;
+    Ok(contacts)
 }

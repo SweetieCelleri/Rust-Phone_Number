4e27e54c7983b0aba90f2d12be31a7070a7686ea
Lucas MIALON <lulum366@gmail.com>
2026-05-03 15:23:50 +0200

    feat(trie): Add convert to PlantUml functions

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use rust_phone_number::parser::load_contacts_from_file;
 use rust_phone_number::trie::Trie;
 
 fn main() {
-    let contacts = load_contacts_from_file("data/01_simple.json")
+    let contacts = load_contacts_from_file("data/04_common_parts.json")
         .expect("Failed to load contacts");
 
     let mut trie = Trie::new();
@@ -11,5 +11,5 @@ fn main() {
         trie.insert(&contact.nb, &contact.name);
     }
 
-    println!("{:?}", trie.collect_all());
+    println!("{}", trie.to_plantuml());
 }
